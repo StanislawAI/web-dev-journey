31d6213e6ef44a155f19d439c725da1afa796f0c
StanislawAI <sortonowski@gmail.com>
2025-04-12 13:20:16 +0200

    First lesson

---
+++ lesson-01-basics/script.js
@@ -0,0 +1,14 @@
+function greet() {
+    // alert("You just clicked a button! 🚀");
+
+    let name = prompt('What is your name?');
+    alert("Nice to meet you " + name + "!");
+
+    let age = prompt('How old are you?');
+    alert("You are " + age + " years old! " + name);
+
+  }
+  
+
+
+
